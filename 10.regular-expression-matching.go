54407d57a00b7f95a584e98aed3f667ef2285608
icesyc <icesyc@qq.ccom>
2022-05-01 11:30:06 +0800

    udate 10 with dp

--- 10.regular-expression-matching.go
+++ 10.regular-expression-matching.go
@@ -5,6 +5,41 @@
  */
 
 // @lc code=start
+/**
+ * DP解法
+ */
+func isMatch(s string, p string) bool {
+	dp := make([][]bool, len(s)+1)
+	for i, _ := range dp {
+		dp[i] = make([]bool, len(p)+1)
+	}
+	dp[0][0] = true
+	for j := 1; j <= len(p); j++ {
+		if p[j-1] == '*' {
+			dp[0][j] = dp[0][j-2]
+		}
+	}
+	for i := 1; i <= len(s); i++ {
+		for j := 1; j <= len(p); j++ {
+			if s[i-1] == p[j-1] || p[j-1] == '.' {
+				dp[i][j] = dp[i-1][j-1]
+			} else if p[j-1] == '*' {
+				dp[i][j] = dp[i][j-2]
+				if s[i-1] == p[j-2] || p[j-2] == '.' {
+					dp[i][j] = dp[i][j] || dp[i-1][j]
+				}
+			} else {
+				dp[i][j] = false
+			}
+		}
+	}
+	return dp[len(s)][len(p)]
+}
+
+//*/
+/**
+ * 解法1
+ *
 func isMatch(s string, p string) bool {
 	i, j, lens, lenp := 0, 0, len(s), len(p)
 	for j = 0; j < lenp; i, j = i+1, j+1 {
@@ -37,6 +72,6 @@ func isMatch(s string, p string) bool {
 	}
 	return i == lens
 }
-
+//*/
 // @lc code=end
 
